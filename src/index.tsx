a4f5c2c832a0fe6904fadb06592a8db5ed086f1c
Mickck <mikemwkim@gmail.com>
2022-10-27 02:26:31 +1100

    useMotionValue, useTransForm, useScroll, SVG

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { darkTheme } from "./theme";
